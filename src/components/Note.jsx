a9080790991e7dfdf24ad77504c0614429b80156
Gogo602 <gogomarosi61@gmail.com>
2025-11-06 18:28:10 +0300

    Priority borderColor

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,20 +1,27 @@
+import { FaTrash } from "react-icons/fa";
+
 
 
 export default function Note({ notes, handleDelete }) {
   
-  
   return (
     <section className="my-5">
        {notes.map((note) => (
-        <div key={note.id} className="border-l-5 border-[#1c0333] text-black  rounded-md bg-white p-3 mb-5 space-y-2">
+         <div key={note.id} className="border-l-5  text-black  rounded-md bg-white p-3 mb-5 space-y-2"
+           style={{
+             borderLeftColor:
+               note.priority === 'High'
+                ? "red"
+                : note.priority === 'Medium'
+                ? 'yellow' 
+                : 'green'
+           }}
+         >
             <div className="flex items-center justify-between">
               <h2 className="font-bold">Title: {note.title}</h2>
               <div className="flex items-center gap-3">
-                <button>
-                  Edit
-                </button>
                 <button onClick={() => handleDelete(note.id)} className="text-red-500">
-                  Delete
+                  <FaTrash  />
                 </button>
               </div>
             </div>
